569c4875224f633fd492c46f2c070f3658963552
Mikhail Abramov <beavisabra@yandex.ru>
2019-09-28 18:36:49 +0400

    Мосив, картинки, кнопки, Андрею мосив

--- main.cpp
+++ main.cpp
@@ -12,17 +12,23 @@ int main()
     HDC pic = txLoadImage("img\\Houses\\Hospital.bmp");
     HDC pik = txLoadImage("img\\Houses\\LowBlueHome.bmp");
     HDC pil = txLoadImage("img\\Houses\\LowGreenHome.bmp");
+    HDC pit = txLoadImage("img\\Houses\\LowYellowHome.bmp");
+    HDC pin = txLoadImage("img\\Houses\\TallBlueHome.bmp");
+    HDC pim = txLoadImage("img\\Houses\\TallGreenHome.bmp");
 
     HDC fon = txLoadImage ("img\\fon.bmp");
     HDC skver = txLoadImage ("img\\skver.bmp");
 
-	ButtonText b1 =  {{20, 10, 120, 50}, "Начать", RGB(255, 0, 0), RGB(0, 0, 0), RGB(255, 0, 0)};
-    ButtonText b2 =  {{20, 60, 120, 100}, "Дома", RGB(255, 0, 0), RGB(0, 0, 0), RGB(255, 0, 0)};
-    ButtonText b5 =  {{20, 110, 120, 150}, "Фонтан", RGB(255, 0, 0), RGB(0, 0, 0), RGB(255, 0, 0)};
-    ButtonText b8 =  {{20, 160, 120, 200}, "Машина", RGB(255, 0, 0), RGB(0, 0, 0), RGB(255, 0, 0)};
-    ButtonText b10 = {{20, 210, 120, 250}, "Госпиталь", RGB(255, 0, 0), RGB(0, 0, 0), RGB(255, 0, 0)};
-    ButtonText b11 = {{20, 260, 120, 300}, "Снеговик", RGB(255, 0, 0), RGB(0, 0, 0), RGB(255, 0, 0)};
-    ButtonText b12 = {{20, 310, 120, 350}, "Единорог", RGB(255, 0, 0), RGB(0, 0, 0), RGB(255, 0, 0)};
+
+
+    ButtonText buttons [7];
+	buttons[0] =  {{20, 10, 120, 50}, "Начать", RGB(255, 0, 0), RGB(0, 0, 0), RGB(255, 0, 0)};
+    buttons[1] =  {{20, 60, 120, 100}, "Дома", RGB(255, 0, 0), RGB(0, 0, 0), RGB(255, 0, 0)};
+    buttons[2] =  {{20, 110, 120, 150}, "Фонтан", RGB(255, 0, 0), RGB(0, 0, 0), RGB(255, 0, 0)};
+    buttons[3] =  {{20, 160, 120, 200}, "Машина", RGB(255, 0, 0), RGB(0, 0, 0), RGB(255, 0, 0)};
+    buttons[4] = {{20, 210, 120, 250}, "Госпиталь", RGB(255, 0, 0), RGB(0, 0, 0), RGB(255, 0, 0)};
+    buttons[5] = {{20, 260, 120, 300}, "Снеговик", RGB(255, 0, 0), RGB(0, 0, 0), RGB(255, 0, 0)};
+    buttons[6] = {{20, 310, 120, 350}, "Единорог", RGB(255, 0, 0), RGB(0, 0, 0), RGB(255, 0, 0)};
 
 	while (!GetAsyncKeyState(VK_ESCAPE)) {
 		txBegin();
@@ -30,20 +36,63 @@ int main()
 		txSetFillColor(RGB(255, 255, 255));
 		txClear();
 
-        Win32::TransparentBlt (txDC(),200,0,600,600,fon,0,0, 123,124, TX_WHITE);
+        Win32::TransparentBlt (txDC(),150,0,700,700,fon,0,0, 123,124, TX_WHITE);
         Win32::TransparentBlt (txDC(),300,200,200,160,skver,0,0, 175,110, TX_WHITE);
 
-		drawButton(b1);
-		drawButton(b2);
-		drawButton(b5);
-		drawButton(b8);
-		drawButton(b10);
-		drawButton(b11);
-		drawButton(b12);
+        for (int  n_button = 0; n_button < 6; n_button++)
+        {
+          drawButton(buttons[n_button]);
+        }
 
-		txBitBlt (txDC(), 700, 10, 100, 100, pic, 0, 0);
-		txBitBlt (txDC(), 700, 120, 100, 100, pik, 0, 0);
-		txBitBlt (txDC(), 700, 230, 100, 100, pil, 0, 0);
+          if (txMouseX() > 20 and
+              txMouseX() < 120 and
+              txMouseY() > 60 and
+              txMouseY() < 100 and
+              txMouseButtons() &1
+              )
+
+        txBitBlt (txDC(), 700, 10, 100, 100, pic, 0, 0);
+
+        if (txMouseX() > 20 and
+              txMouseX() < 120 and
+              txMouseY() > 60 and
+              txMouseY() < 100 and
+              txMouseButtons() &1
+              )
+
+		txBitBlt (txDC(), 700, 100, 100, 100, pik, 0, 0);
+
+		if (txMouseX() > 20 and
+              txMouseX() < 120 and
+              txMouseY() > 60 and
+              txMouseY() < 100 and
+              txMouseButtons() &1
+              )
+		txBitBlt (txDC(), 700, 190, 100, 100, pil, 0, 0);
+
+		if (txMouseX() > 20 and
+              txMouseX() < 120 and
+              txMouseY() > 60 and
+              txMouseY() < 100 and
+              txMouseButtons() &1
+              )
+		txBitBlt (txDC(), 700, 280, 100, 100, pit, 0, 0);
+
+		if (txMouseX() > 20 and
+              txMouseX() < 120 and
+              txMouseY() > 60 and
+              txMouseY() < 100 and
+              txMouseButtons() &1
+              )
+		txBitBlt (txDC(), 700, 350, 100, 100, pin, 0, 0);
+
+		if (txMouseX() > 20 and
+              txMouseX() < 120 and
+              txMouseY() > 60 and
+              txMouseY() < 100 and
+              txMouseButtons() &1
+              )
+		txBitBlt (txDC(), 700, 460, 100, 100, pim, 0, 0);
 
 		txSleep(10);
 	}
